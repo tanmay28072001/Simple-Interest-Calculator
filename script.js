856cde05e38495ed1133eccaa0bc5b86d6f02926
tanmay28072001 <tanmaypatel28072001@gmail.com>
2021-05-27 07:03:48 +0000

    changes made from theia lab environment

---
+++ script.js
@@ -0,0 +1,13 @@
+var principal=document.getElementById("principal").value;
+var rate=document.getElementById("rate").value;
+var years=document.getElementById("years").value;
+var interest = principal * years * rate / 100 ;
+var year=new Date().getFullYear()+parseInt(years);
+function updateRate(){
+    var rateval=document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML=rateval;
+}
+function compute(){
+    var answer="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+    document.getElementById("result").innerHTML=answer;
+}
